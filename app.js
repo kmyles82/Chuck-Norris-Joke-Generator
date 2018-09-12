69cdec4e4b4e5aa923319a484c923f3ed8062358
kmyles82 <kmyles82@gmail.com>
2018-09-12 15:06:01 -0500

    completed projected to use ajax to get data from external api

--- app.js
+++ app.js
@@ -11,7 +11,19 @@ function getJokes(e) {
         if (this.status === 200) {
             // console.log(this.responseText)
             const response = JSON.parse(this.responseText);
-            console.log(response)
+            // console.log(response)
+
+            let output = '';
+
+            if (response.type === 'success') {
+                response.value.forEach(function (joke) {
+                    output += `<li>${joke.joke}</li>`;
+                });
+            } else {
+                output += `<li>Something went wrong</li>`;
+            }
+
+            document.querySelector('.jokes').innerHTML = output;
         }
     }
 
